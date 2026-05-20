e4fb2cbed60704f290eed30b64e1399f9c4cec87
wesley <wzbin2@gmail.com>
2026-05-20 10:47:10 -0700

    fixed stuff

--- scripts/dummy.gd
+++ scripts/dummy.gd
@@ -3,23 +3,23 @@ extends ActiveRagdoll
 
 class_name Dummy
 
-@export var face_texture: Texture2D#:
-	#set(val):
-		#face_texture = val
-		#if Engine.is_editor_hint():
-			#update_stuff()
+@export var face_texture: Texture2D:
+	set(val):
+		face_texture = val
+		if Engine.is_editor_hint():
+			update_stuff()
 
-@export var face_texture_scale: Vector3 = Vector3(1, 1, 1)#:
-	#set(val):
-		#face_texture_scale = val
-		#if Engine.is_editor_hint():
-			#update_stuff()
+@export var face_texture_scale: Vector3 = Vector3(1, 1, 1):
+	set(val):
+		face_texture_scale = val
+		if Engine.is_editor_hint():
+			update_stuff()
 
-@export var text: String#:
-	#set(val):
-		#text = val
-		#if Engine.is_editor_hint():
-			#update_stuff()
+@export var text: String:
+	set(val):
+		text = val
+		if Engine.is_editor_hint():
+			update_stuff()
 
 @onready var face_sprite: Sprite3D = $"PhysicsGuy/Armature/Skeleton3D/PhysicalBoneSimulator3D/Physical Bone Head/Sprite3D"
 @onready var text_label: Label3D = $"PhysicsGuy/Armature/Skeleton3D/PhysicalBoneSimulator3D/Physical Bone Head/Label3D"
@@ -30,12 +30,9 @@ func _ready() -> void:
 	update_stuff()
 	bones_to_ignore.append_array([$"PhysicsGuy/Armature/Skeleton3D/PhysicalBoneSimulator3D/Physical Bone Upper Arm_R", $"PhysicsGuy/Armature/Skeleton3D/PhysicalBoneSimulator3D/Physical Bone Upper Arm_L", $"PhysicsGuy/Armature/Skeleton3D/PhysicalBoneSimulator3D/PhysicalBone Forearm_R", $"PhysicsGuy/Armature/Skeleton3D/PhysicalBoneSimulator3D/PhysicalBone Forearm_L"])
 
-
 func update_stuff() -> void:
-	print("gfghjhgchcjvkbhvcgj")
 	if face_texture:
 		face_sprite.texture = face_texture
-		print("fzdxghkbjvdzgxfhk")
 	else:
 		face_sprite.texture = null
 	
