35928c63321e1911d80e46158ef1bddfec30b94f
wesley <wzbin2@gmail.com>
2026-05-28 10:08:51 -0700

    stuff

--- levels/world.gd
+++ levels/world.gd
@@ -4,11 +4,11 @@ class_name World
 
 @export var spawn_orgin_marker: Marker3D
 
-var water: Array[Node]
+var water: Array[Node] = []
 
 var spawn_pos: Vector3:
 	get():
 		return spawn_orgin_marker.global_position if spawn_orgin_marker else Vector3.ZERO
 
-func _ready() -> void:
-	water = get_tree().get_nodes_in_group("water")
+#func _ready() -> void:
+	#water = get_tree().get_nodes_in_group("water")
---
+++ scripts/waiter.gd
@@ -0,0 +1,5 @@
+extends Dummy
+
+func _ready() -> void:
+	super._ready()
+	bones_to_ignore = []
